561e8130a0a5ac96ef4926fa93603c9ed33e89f8
ifthakhar94 <ifthakhar8@gmail.com>
2023-03-07 08:57:24 +0600

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+// React BootStrap
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
